5eb211f9b70fd7535a28534ed115695dd3f38625
Sujay KS <42107965+sujay2306@users.noreply.github.com>
2025-01-19 13:21:10 +0530

    added write and read connection

--- main.go
+++ main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 
-func do(conn, net.Conn){
+func do(conn net.Conn){
+
 	buf := make ([] byte, 1024)
- 	conn.Read(buf)
-	conn.Write()
+ 	_,err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(1 * time.Second) //wait for 1 sec
+	conn.Write([] byte ("HTTP/1.1 200 OK\r\n\r\nHello I'm TCP Server!!!\r\n"))
 	conn.Close()
 }
+
 func main(){
 	listener, err := net.Listen("tcp", ":1729")
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	conn , err := listener.Accept()
+	conn,err := listener.Accept()
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	do(conn)
 
 }
